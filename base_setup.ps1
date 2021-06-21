93a6efc609133b8606e73bfddee46930ec60668b
Tiernan OToole <tiernan@tiernanotoole.ie>
2021-06-21 23:27:55 +0100

    Create base_setup.ps1

---
+++ base_setup.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+install-module 'AuditPolicyDsc','SecurityPolicyDsc','NetworkingDsc','xPSDesiredStateConfiguration','xNetworking','xWebAdministration',IISAdministration -force
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiernano/powershell-dsc-scripts/main/CSBP_WinServer2019.ps1'))
+
+
