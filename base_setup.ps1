8b4d203abe7f8d212299702e0e7660791ac0d531
Tiernan OToole <tiernan@tiernanotoole.ie>
2021-06-28 15:29:32 +0100

    tweaking base

--- base_setup.ps1
+++ base_setup.ps1
@@ -5,5 +5,5 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 install-module 'AuditPolicyDsc','SecurityPolicyDsc','NetworkingDsc','xPSDesiredStateConfiguration','xNetworking','xWebAdministration',IISAdministration -force
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiernano/powershell-dsc-scripts/main/CSBP_WinServer2019.ps1'))
-
-
+CSBP_WindowsServer2019
+Start-DscConfiguration -Path .\CSBP_WindowsServer2019  -Force -Verbose -Wait
